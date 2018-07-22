82793fc75c444b18b26943aed57fdc6fdfe7f7c2
Walter Santiago <wsantiagog1@gmail.com>
2018-07-22 17:50:48 -0500

    35. Unit Testing en Laravel

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,22 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        //Pasos para un Test
+
+        //Paso 1:  Arrange | Preparación
+
+        //Paso 2: Act | Action
+
+        //Paso 3: Assert | Verificación
         $response = $this->get('/');
 
         $response->assertStatus(200);
+        $response->assertSee('Laratter');
+    }
+
+    public function testCanSearchForMessages()
+    {
+        $response = $this->get('/messages?query=Alice'); 
+        $response->assertSee('Alice');
     }
 }
